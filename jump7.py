920a7033236ed0bc684df62137f8a5d789f13e24
lj <1421221551@qq.com>
2020-04-08 13:08:19 +0800

    study work20200408_2

---
+++ jump7.py
@@ -0,0 +1,12 @@
+#7jump-20200407
+i = 1
+for i in range(1,101):    
+    if i % 7 == 0:
+        continue
+    elif i % 10 == 7:
+        continue
+    elif i // 10 == 7:
+        continue
+    else:
+        print (i) 
+
